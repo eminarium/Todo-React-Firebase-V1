787551f59b20fff52c6f178439c7874fcc71e4e5
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2020-12-12 21:22:24 +0500

    General template is ready with basic functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { 
+  FormControl, 
+  InputLabel, 
+  Input, 
+  Button 
+} from '@material-ui/core'
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(["First task", "second task"]);
+  const [input, setInput] = useState('')
+
+  const addTodo = (event) => {
+    // Prevent default behaviour of a form (Prevent page refresh)
+    event.preventDefault();
+
+    // Add current value of input as a new todo
+    setTodos([...todos, input]);
+
+    // Clear up input field after submit
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Coders!</h1>
+      
+      <form>
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange = {(event) => setInput(event.target.value)} />
+        </FormControl>
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={addTodo}>
+          Add Todo
+        </Button>
+
+        <ul>
+          {
+            todos.map(todo => 
+              <li key={todo}>{todo}</li>
+            )
+          }
+        </ul>
+      </form>
     </div>
   );
 }
