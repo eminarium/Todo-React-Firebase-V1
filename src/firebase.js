5f29a7fabaf8145aae303fec3967ead3ea7f25c5
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2020-12-13 18:44:42 +0500

    Hides Firebase API key in a separate file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase'
+import FIREBASE_API_KEY from './env_variables'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDhDdgnQEtUunLgqcR3v8BHx3ZSjFs4LcU",
+    //apiKey: "AIzaSyDhDdgnQEtUunLgqcR3v8BHx3ZSjFs4LcU",
+    apiKey: FIREBASE_API_KEY,
     authDomain: "todo-app-v1-tdv1.firebaseapp.com",
     projectId: "todo-app-v1-tdv1",
     storageBucket: "todo-app-v1-tdv1.appspot.com",
